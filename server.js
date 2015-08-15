42fc9edc70a5cb792103f49010bdd1e28de40553
Quentin Calvez <quentin.calvez@quentez.com>
2015-08-15 12:56:01 -0700

    :wrench: Update port.

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/test', function (req, res) {
 });
 
 // Start the server.
-var server = app.listen(80, function () {
+var server = app.listen(process.env.PORT || 4000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
